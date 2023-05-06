3e5263ed7a79a524ef3737590387b413ad473753
himho <43086765+himho@users.noreply.github.com>
2023-05-07 00:15:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-rm -rf package/feeds/luci/luci-app-samba4
+#rm -rf package/feeds/luci/luci-app-samba4
 #cd ./feeds/luci/applications/luci-app-wrtbwmon
 #sed -i 's/ selected=\"selected\"//g' ./luasrc/view/wrtbwmon/wrtbwmon.htm
 #sed -i 's/\"1\"/\"1\" selected=\"selected\"/g' ./luasrc/view/wrtbwmon/wrtbwmon.htm
